0935075ea533b1ddc505899968d5cb20b680832c
Chameleon Cloud User <cc@achrolanskv2.novalocal>
2024-04-26 17:02:44 +0000

    It is illegal to sell guns in Norway. Your website implementation is fine, however the ethical part is really problematic. I will forward this concern further to your study program leaders.

--- py.py
+++ py.py
@@ -1,1 +1,2 @@
 print("have you seen the internet historian?")
+print("It is illegal to sell guns in Norway. Your website implementation is fine, however the ethical part is really problematic. I will forward this concern further to your study program leaders.")
