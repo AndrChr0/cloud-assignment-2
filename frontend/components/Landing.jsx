6eb245386666501b4a5b4818b5f1fe0725495c0f
root <root@achro.novalocal>
2024-05-20 15:44:05 +0000

    removed useless fetch

--- frontend/components/Landing.jsx
+++ frontend/components/Landing.jsx
@@ -3,26 +3,12 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Landing = () => {
-  const [userCount, setUserCount] = useState(0);
-  const api = import.meta.env.VITE_URL;
-
-  useEffect(() => {
-    axios
-      .get(`${api}/`)
-      .then((response) => {
-        setUserCount(response.data.user_count);
-      })
-      .catch((error) => {
-        console.error("There was an error fetching the data!", error);
-      });
-  }, []);
 
   return (
     <div>
       <h1>Welcome to fake reddit</h1>
         <p>Your place for posting posts and liking them. </p>
         <Link to="/register">Register here</Link>
-        <p>There are currently {userCount} users on the platform.</p>
     </div>
   );
 };
