7fe41c69f30b30ed1812a26b21677c70c1d318d2
chriistopherngo <ngo.chris@hotmail.com>
2024-05-22 17:44:40 +0200

    ferdig me styling i zink

--- frontend/components/Nav.jsx
+++ frontend/components/Nav.jsx
@@ -14,41 +14,59 @@ const Nav = () => {
   return (
     <nav>
       <ul>
-        {isLoggedIn && (
-          <>
-            <div className="homeNewPostAndCategory">
-              <li>
-                {isLoggedIn ? (
-                  <Link to="/home">Home</Link>
-                ) : (
-                  <Link to="/">Home</Link>
-                )}
-              </li>
+        <div className="homeNewPostAndCategory">
+          <li>
+            {isLoggedIn ? (
+              <Link to="/home">FakeReddit</Link>
+            ) : (
+              <Link to="/">Home</Link>
+            )}
+          </li>
+        </div>
+        <div className="profileAndLog">
+          {isLoggedIn ? (
+            <>
               <li>
                 <Link to="/new-category">New Category</Link>
               </li>
               <li>
                 <Link to="/new-post">New Post</Link>
               </li>
-            </div>
-
-            <div className="profileAndLog">
               <li>
                 <Link to="/profile">Profile</Link>
               </li>
+              {isLoggedIn && <li>|</li>}
+            </>
+          ) : (
+            ""
+          )}
 
-              <li>
-                {isLoggedIn ? (
-                  <Link onClick={logOut} to="/">
-                    Log out
-                  </Link>
-                ) : (
+          <li>
+            {isLoggedIn ? (
+              <Link onClick={logOut} to="/" style={{ color: "red" }}>
+                Log out
+              </Link>
+            ) : (
+              <>
+                <div
+                  style={{ display: "flex", gap: "10px", alignItems: "center" }}
+                >
                   <Link to="/login">Log in</Link>
-                )}
-              </li>
-            </div>
-          </>
-        )}
+                  <Link
+                    to="/register"
+                    style={{
+                      border: "2px solid #007bff",
+                      padding: "4px 8px",
+                      borderRadius: "8px",
+                    }}
+                  >
+                    Sign up
+                  </Link>
+                </div>
+              </>
+            )}
+          </li>
+        </div>
       </ul>
     </nav>
   );
